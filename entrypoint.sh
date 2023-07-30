ad404cb8d0161c3977171f059b73c6c967866fc9
Ian Martin <ian.martin@redcanary.com>
2023-07-30 17:04:05 -0600

    shell format linting

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,28 +4,28 @@ called_as=$1
 shift
 
 case $called_as in
-    upsdrvctl)
-        exec upsdrvctl -u nut -D start "$@"
-        ;;
-    upsd)
-        exec upsd -u nut -D "$@"
-        ;;
-    upsmon)
-        exec upsmon -u nut -D "$@"
-        ;;
-    exporter)
-        exec nut_exporter --nut.vars_enable="" --web.listen-address=":9199"
-        ;;
-    list-drivers)
-        exec ls -1 --color=never /usr/lib/nut
-        sleep 5
-        ;;
-    scan)
-        exec nut-scanner -N
-        ;;
-    *)
-        echo "Unknown Command: '$called_as'"
-        sleep 10
-        exit 3
-        ;;
+upsdrvctl)
+	exec upsdrvctl -u nut -D start "$@"
+	;;
+upsd)
+	exec upsd -u nut -D "$@"
+	;;
+upsmon)
+	exec upsmon -u nut -D "$@"
+	;;
+exporter)
+	exec nut_exporter --nut.vars_enable="" --web.listen-address=":9199"
+	;;
+list-drivers)
+	exec ls -1 --color=never /usr/lib/nut
+	sleep 5
+	;;
+scan)
+	exec nut-scanner -N
+	;;
+*)
+	echo "Unknown Command: '$called_as'"
+	sleep 10
+	exit 3
+	;;
 esac
